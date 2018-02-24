c4038d04f40a923a5a9b77b6fc4eeabd601d5f5c
stroppa <stroppa@MacBook-Pro-de-stroppa.local>
2018-02-24 22:09:56 +0100

    better lazy loading photos

--- client/src/Components/Photo.js
+++ client/src/Components/Photo.js
@@ -10,31 +10,18 @@ class Photo extends React.Component {
 		  error  : false
 	  }
   }
-  
-  componentDidMount() {
-	  this.loadImage().then(() => this.setState({loaded : true}))
-  }
-  
-  loadImage() {
-	  return new Promise((resolve, reject) => {
-	    const image = new Image();
-	    image.onload = () => resolve();
-	    image.onerror = err => reject(err);
-	    image.src = this.props.url_m;
-	  });
-  }	
-	
+
   render() {
 	  
 	const { ownername,url_t,url_m,url_l,description,date_taken,views,title } = this.props;  
 	  
     return (
       <div className="App-photo" onClick={() => this.props.onClick()} >
-	  	 <img className={"App-photo-img "+(!this.state.loaded?"App-photo-img--blur":"")} src={this.state.loaded?url_m:url_t} />
+	  	 <img className={"App-photo-img App-photo-img--blur"+(this.state.loaded?" App-photo-img--hide":"")} src={url_t} />
+	  	 <img className={"App-photo-img "+(this.state.loaded?" App-photo-img--show":"App-photo-img--hide")} src={url_m} onLoad={() => this.setState({loaded : true})}/>
 	  	 <div className="App-photo-description">
 	  	 	<h2 className="App-photo-caption">{title}</h2>
 	  	 	<h2 className="App-photo-username">{ownername}</h2>
-	  	 	
 	  	 	
 	  	 </div>
 	  </div> 
