6c002ac34e6fb8ad2b7f9724d77cc26328837f4c
stroppa <stroppa@MacBook-Pro-de-stroppa.local>
2018-02-24 14:49:11 +0100

    add username on hover photo

--- client/src/Components/Photo.js
+++ client/src/Components/Photo.js
@@ -26,12 +26,14 @@ class Photo extends React.Component {
 	
   render() {
 	  
-	const { owner_name,url_t,url_m,url_l,description,date_taken,views } = this.props;  
+	const { ownername,url_t,url_m,url_l,description,date_taken,views } = this.props;  
 	  
     return (
       <div className="App-photo">
-	  	 {<img className={"App-photo-img "+(!this.state.loaded?"App-photo-img--blur":"")}
-		 	   src={this.state.loaded?url_m:url_t} /> }
+	  	 <img className={"App-photo-img "+(!this.state.loaded?"App-photo-img--blur":"")} src={this.state.loaded?url_m:url_t} />
+	  	 <div className="App-photo-description">
+	  	 	<h2 className="App-photo-username">{ownername}	</h2>
+	  	 </div>
 	  </div> 
     );
   }
