a9bd2c814e03933967ebd396d9a046b4fe3dd77c
stroppa <stroppa@MacBook-Pro-de-stroppa.local>
2018-02-25 17:55:36 +0100

    add lazy load to lightbox

--- client/src/Components/LazyImage/LazyImage.js
+++ client/src/Components/LazyImage/LazyImage.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import '../../css/App.css'; 
-
 import '../../css/LazyImage/LazyImage.css';
 
 class LazyImage extends React.Component {
@@ -15,11 +13,16 @@ class LazyImage extends React.Component {
   render() {
 	  
     const {thumbsrc,src,className} = this.props
-
+	let additionalClass=typeof className !== 'undefined'?className:"";
+	
     return (
     	<div className="Lazy-image">
-   		  <img className={" Lazy-image-item Lazy-image-item--blur"+(this.state.loaded?" Lazy-image-item--hide":"")} src={thumbsrc} />
-   		  <img className={" Lazy-image-item "+(this.state.loaded?" Lazy-image-item--show":"Lazy-image-item--hide")} src={src} onLoad={() => 					this.setState({loaded : true})}/>
+   		  <img className={additionalClass+" Lazy-image-item Lazy-image-item--blur"+(this.state.loaded?" Lazy-image-item--hide":"")} 
+   		  	   src={thumbsrc} />
+   		  <img className={additionalClass+" Lazy-image-item "+(this.state.loaded?" Lazy-image-item--show":"Lazy-image-item--hide")} 
+   		       src={src} onLoad={() => this.setState({loaded : true})}
+   		        		       
+   		       />
    		</div>
     );
   }
--- client/src/Components/LightBox/LightBox.js
+++ client/src/Components/LightBox/LightBox.js
@@ -1,38 +1,63 @@
 import React from 'react'; 
 import Modal from './Modal'
-import '../../css/LightBox/LightBox.css'; 
+import LazyImage from '../LazyImage/LazyImage'
+import '../../css/LightBox/LightBox.css';
+ 
  
 
 class LightBox extends React.Component {
 	
   constructor(props) {
 	  super(props)
+	  this.highResImage = {
+		  highResUrl : '',
+		  highResHeight : '100%',
+		  highResWidth : '100%',
+	  }
+  
   }
   
-  componentDidMount() {
-
-  }
+  setMaxResImage() {
+	  
+	let highResPhoto,highResWidth,highResHeight;
 	
+	const images = ["url_l", "url_c", "url_m"];
+	
+	for(var i = 0; i < images.length; i++) {
+		if(this.props.photo.hasOwnProperty(images[i])) {
+			let type = images[i].split('_')[1];
+			return { highResUrl    : this.props.photo["url_"+type],
+					 highResHeight : this.props.photo["height_"+type]+'px',
+					 highResWidth : this.props.photo["width_"+type]+'px',
+					}	
+		}
+	}
+  }
+  	
   render() {
 	
 	const { isOpen,photo } = this.props;    
-	 console.log(photo)
+	let highResImage = {}
+	if (isOpen)	
+		highResImage = this.setMaxResImage()
+	
     return (
       <React.Fragment>
       	<Modal isOpen={isOpen} onRequestClose={() => this.props.onRequestClose()} >
-      		<div className="LightBox">
-	      		<div className="LightBox-image">
-	      			<img src={photo.url_l} style={{height:photo.height_l+'px'}}/>
+      	  {isOpen && 
+      	 	<div className="LightBox">
+      			
+	      		<div className="LightBox-image" style={{height:`${highResImage.highResHeight}`,width:`${highResImage.highResWidth}` }}>
+	      			<LazyImage  className="LightBox-lazy-item" thumbsrc={photo.url_m} src={highResImage.highResUrl} height={photo.height_l} />
 		  		</div>
 		  		<div className="LightBox-description">
 		  			<h2 className="LightBox-description-text LightBox-description-text--right">{photo.title}</h2> 
 		  			<h2 className="LightBox-description-text LightBox-description-text--gray LightBox-description-text--left">{photo.ownername}</h2>
 		  			<h2 className="LightBox-description-text LightBox-description-text--gray LightBox-description-text--right">{(typeof photo.datetaken!= "undefined"?photo.datetaken.slice(0,10):"")}</h2>
 		  			<h2 className="LightBox-description-text LightBox-description-text--left"> {photo.views} views</h2>
-		  			
-			  			
 		  		</div>
-	  		</div>
+		 	</div>
+	  		}
       	</Modal>
       </React.Fragment> 
     );
